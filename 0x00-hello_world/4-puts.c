e9aefa3d31525ff8d81459d428a785833dedd2e9
root <akissi.ngoran@uvci.edu.ci>
2022-03-10 06:37:20 -0800

    Modif file 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#!/bin/bash
-#include<stio.h>
+#include <stdio.h>
 
 /**
- * Prints the phrase "Programming is like building a multilingual puzzle" 
+ * main - prints the phrase "Programming is like building a multilingual puzzle
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building aa multilingual puzzle");
 	return (0);
 }
